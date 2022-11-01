aa7b9ea641f1bca7a48c1c6197f6cf5a9cbbeaec
Ladypatra <patratim@yahoo.com>
2022-11-01 08:42:52 +0000

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '5d0j9j4tsj';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-hgx7t4pw.us.auth0.com',
+  clientId: 'UTfkZzRLVFMc1Qecl94meZJwfamoRY2D',
+  callbackUrl: 'http://localhost:3000/callback'
+};
